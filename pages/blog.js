5021e0e2b5eac46457d935e02751d4aa613a24b3
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-05 20:30:50 +0000

    linking back from blog to home

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function Blog () {
     return (
-        <>
-        Oh hello there, you have made it to the blog!
-        </>
+        <h2>
+        <Link href="/">
+          <a>Click here to return home!</a>
+        </Link>
+      </h2>
     )
 }
