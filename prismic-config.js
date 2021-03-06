dc84d7206f196bc88a7f263eccd5c69924444c5d
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-06 13:01:13 +0000

    prismic config set up

---
+++ prismic-config.js
@@ -0,0 +1,23 @@
+import Prismic from "prismic-javascript"
+
+export const apiEndpoint = "https://sock-butler.cdn.prismic.io/api/v2"
+
+export const accessToken = "";
+
+//create client
+
+export const Client = (req = null) => {
+    Prismic.client(apiEndpoint, createClientOptions(req, accessToken))
+
+    const createClientOptions = (req = null, prismicAccessToken = null) => {
+
+            const reqOption = req ? {req} : {}
+            const accessTokenOption = prismicAccessToken ? {accessToken : prismicAccessToken} : {}
+
+            return {
+                ...reqOption,
+                ...accessTokenOption
+            }
+    }
+}
+
