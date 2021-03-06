115c4d5eb580e13007acaaaffba68c589fbc3195
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-06 20:08:01 +0000

    return to home page from account

--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,18 @@
+import Link from "next/link"
+
 export default function () {
     return (
+        <div>
         <h1>
             welcome to your account!
         </h1>
+        <h2>
+            Click{" "}
+            <Link href="/">
+                <a>here</a>{" "}
+                to return to the home page
+            </Link>
+        </h2>
+        </div>
     )
 }
