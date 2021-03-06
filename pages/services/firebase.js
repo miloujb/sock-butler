773eea28cadb10a3f1dea9d449b65904c6e9e863
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-06 22:05:52 +0000

    solved fs issue

--- pages/services/firebase.js
+++ pages/services/firebase.js
@@ -4,15 +4,29 @@ import loadConfig from "next/dist/next-server/server/config";
 
 
 var firebaseConfig = {
-    apiKey: NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    authDomain: NEXT_FIREBASE_AUTH_DOMAIN,
-    databaseURL: NEXT_PUBLIC_FIREBASE_DB_URL,
-    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID 
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID 
     
   };
 
   export default function initFirebase() {
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig)
+    }
+  }
+
+ 
+  module.exports = {
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+  
+      return config
     }
   }
